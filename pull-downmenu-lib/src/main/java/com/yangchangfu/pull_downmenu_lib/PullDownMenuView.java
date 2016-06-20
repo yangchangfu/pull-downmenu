89b8d061d98773cda3f321b6ce0a25aa355440c1
changfu,Yang <276054866@qq.com>
2016-06-20 19:35:48 +0800

    新增菜单库
    创建菜单库

---
+++ pull-downmenu-lib/src/main/java/com/yangchangfu/pull_downmenu_lib/PullDownMenuView.java
@@ -0,0 +1,23 @@
+package com.yangchangfu.pull_downmenu_lib;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by apple on 16/6/20.
+ */
+public class PullDownMenuView extends LinearLayout {
+
+    public PullDownMenuView(Context context) {
+        super(context);
+    }
+
+    public PullDownMenuView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PullDownMenuView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
